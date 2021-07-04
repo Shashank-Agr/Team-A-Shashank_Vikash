5f4ac18a76813966f0a4cce7841330d909b0357a
shashank-agr <shashaagrawal@deloitte.com>
2021-07-04 18:51:11 +0530

    basic code for all APIs added

--- ECommercePlatform/Flask_Skeletion/Flask_Skeletion/app/controller/controller.py
+++ ECommercePlatform/Flask_Skeletion/Flask_Skeletion/app/controller/controller.py
@@ -7,8 +7,8 @@ from ..services.product_list import *
 from ..services.product_by_ID import *
 
 uri = Blueprint("endpoint", __name__, url_prefix="/")
-uri.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(uri)
+# uri.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# db = SQLAlchemy(uri)
 
 
 # @uri.route("/healthCheck", methods=["GET"])
@@ -19,6 +19,9 @@ db = SQLAlchemy(uri)
 def health_check():
     return "Welcome to E-Commerce Platform"
 
+
+
+cart=dict()
 
 # Task 1: Retrieve all the business products
 
@@ -52,17 +55,66 @@ def fetch_product_by_ID(id):
     return (jsonify(res),200)
     
 
-# Task 3: Add a product to cart​
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+###############  using sku
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
+@uri.route("/products/<id>", methods=["GET"])
+def specificProduct(id):
+    file=open('products.json')
+    data=json.load(file)
+    #product_id=request.args['product_id']
+    for x in data:
+        if x["sku"]==id:
+            return (jsonify(x),200)
+    return ("Product not found",404)
+
+
+# Task 3: Add a product to cart​
+
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(200), nullable=False)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
+
+@uri.route("/addProduct/<int:product_id>", methods=["GET"])
+def addProduct(product_id):  
+    file=open('products.json')
+    data=json.load(file)
+    #product_id=request.args['product_id']
+    for x in data:
+        if x["sku"]==product_id:
+            if product_id in cart:
+                return (jsonify(cart),200)
+            else:
+                cart[product_id]=x
+                return (jsonify(cart),200)
+    return ("Product not found",404)
 
 # Task 4: Delete a product from cart​
 
+@uri.route("/deleteProduct/<int:product_id>", methods=["GET"])
+def deleteProduct(product_id):
+    if product_id in cart:
+        cart.pop(product_id)
+        return (jsonify(cart),200)
+    else:
+        return (jsonify(cart),200)
+
+
 # Task 5: List products from cart​
 
+@uri.route("/listCart", methods=["GET"])
+def listCart():
+    return (jsonify(cart),200)
+
 # Task 6: Proceed to Purchase​
+
+@uri.route("/purchase", methods=["GET"])
+def purchase():
+    total=0
+    for product in cart:
+        total=total+cart[product]["price"]
+    return (jsonify(total),200)
+    
