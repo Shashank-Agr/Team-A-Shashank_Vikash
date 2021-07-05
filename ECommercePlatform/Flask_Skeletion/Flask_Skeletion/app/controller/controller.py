116347b9ee59ef11ded4f2df626b97ebb7bb510e
shashank-agr <shashaagrawal@deloitte.com>
2021-07-05 12:03:34 +0530

    updated ECommerce

--- ECommercePlatform/Flask_Skeletion/Flask_Skeletion/app/controller/controller.py
+++ ECommercePlatform/Flask_Skeletion/Flask_Skeletion/app/controller/controller.py
@@ -1,25 +1,16 @@
 from flask import Flask, Blueprint, jsonify, request, json, render_template, url_for, request, redirect, current_app
 import flask
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 from ..services.product_list import *
 from ..services.product_by_ID import *
+from ..services.add_product import *
 
 uri = Blueprint("endpoint", __name__, url_prefix="/")
-# uri.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-# db = SQLAlchemy(uri)
-
-
-# @uri.route("/healthCheck", methods=["GET"])
-# def healthCheck():
-#     return "Working"
 
 @uri.route("/")
 def health_check():
     return "Welcome to E-Commerce Platform"
-
-
 
 cart=dict()
 
@@ -41,7 +32,7 @@ def fetch_product():
     
 # Task 2: Retrieve details of a specific product based on product_id​
 
-@uri.route("/products/<id>", methods = ["GET"])
+@uri.route("/products/<int:id>", methods = ["GET"])
 def fetch_product_by_ID(id):
     product = get_product_by_id(id)
     res = dict()
@@ -53,43 +44,21 @@ def fetch_product_by_ID(id):
         res['product_list'] = []
 
     return (jsonify(res),200)
-    
-
-###############  using sku
-
-@uri.route("/products/<id>", methods=["GET"])
-def specificProduct(id):
-    file=open('products.json')
-    data=json.load(file)
-    #product_id=request.args['product_id']
-    for x in data:
-        if x["sku"]==id:
-            return (jsonify(x),200)
-    return ("Product not found",404)
-
 
 # Task 3: Add a product to cart​
 
-# class Todo(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     content = db.Column(db.String(200), nullable=False)
-#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-#     def __repr__(self):
-#         return '<Task %r>' % self.id
-
 @uri.route("/addProduct/<int:product_id>", methods=["GET"])
 def addProduct(product_id):  
-    file=open('products.json')
-    data=json.load(file)
-    #product_id=request.args['product_id']
-    for x in data:
-        if x["sku"]==product_id:
-            if product_id in cart:
-                return (jsonify(cart),200)
-            else:
-                cart[product_id]=x
-                return (jsonify(cart),200)
+    product = add_to_cart(product_id)
+    res = dict()
+    if(len(product)!=0):
+        if product_id in cart:
+            return (jsonify(cart),200)
+        else:
+            cart[product_id]=product
+
+        return (jsonify(cart),200)
+
     return ("Product not found",404)
 
 # Task 4: Delete a product from cart​
@@ -116,5 +85,5 @@ def purchase():
     total=0
     for product in cart:
         total=total+cart[product]["price"]
-    return (jsonify(total),200)
+    return (f'Cart Total: {total}',200)
     
--- ECommercePlatform/Flask_Skeletion/Flask_Skeletion/app/services/add_product.py
+++ ECommercePlatform/Flask_Skeletion/Flask_Skeletion/app/services/add_product.py
@@ -1,10 +1,10 @@
 import json
 
-def get_product_by_id(product_id):
+def add_to_cart(product_id):
     with open('./products.json','r') as jsonfile:
         file_data = json.loads(jsonfile.read())
-
     for x in file_data:
         if x["sku"]==product_id:
+            #x['count'] = 1
             return x
     return {}
